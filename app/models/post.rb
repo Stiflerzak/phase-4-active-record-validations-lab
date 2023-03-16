44279439cc9b02c0a359a37fd4fc0f9b0b6fffe9
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-16 18:17:57 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validate :title_clickbaity
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion:{in: %w(Fiction Non-Fiction)}
+
+    def title_clickbaity
+        if title.present? && !title.include?("Won't Believe") && !title.include?("Secret") && !title.match(/Top \d+/) && !title.include?("Guess")
+          errors.add(:title, 'should be clickbait-y')
+        end
+      end
 end
